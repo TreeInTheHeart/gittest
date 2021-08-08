7405f4d732d87baf543549a011b8e055a5564bf8
gs <964016262@qq.com>
2021-08-08 17:12:03 +0800

    second commit

--- src/main/java/com/atgs/git/GitTest.java
+++ src/main/java/com/atgs/git/GitTest.java
@@ -2,6 +2,7 @@ package com.atgs.git;
 
 public class GitTest {
     public static void main(String[] args) {
+        System.out.println("hello git2");
         System.out.println("hello git");
     }
 }
